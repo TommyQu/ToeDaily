24cec0933674468710ada7da21d8619e1c99580b
Tommy Qu <tommyqu1992@gmail.com>
2016-04-19 19:15:21 -0400

    Add login and sign up page

---
+++ www/js/controller/signUpCtrl.js
@@ -0,0 +1,32 @@
+app.controller('signUpCtrl', function($scope, $filter, ionicDatePicker) {
+    var ipObj1 = {
+      callback: function (val) {  //Mandatory
+      	var x = $filter('date')(new Date(val), "yyyy-MM-dd");;
+      	$scope.dob = x;
+      },
+      disabledDates: [            //Optional
+        new Date(2016, 2, 16),
+        new Date(2015, 3, 16),
+        new Date(2015, 4, 16),
+        new Date(2015, 5, 16),
+        new Date('Wednesday, August 12, 2015'),
+        new Date("08-16-2016"),
+        new Date(1439676000000)
+      ],
+      from: new Date(2012, 1, 1), //Optional
+      to: new Date(2016, 10, 30), //Optional
+      inputDate: new Date(),      //Optional
+      mondayFirst: true,          //Optional
+      disableWeekdays: [0],       //Optional
+      closeOnSelect: false,       //Optional
+      templateType: 'popup'       //Optional
+    };
+
+    $scope.openDatePicker = function(){
+      ionicDatePicker.openDatePicker(ipObj1);
+    };
+
+    $scope.signUp = function() {
+    	alert("asd");
+    };
+});
