5ea48bef5fe28ed7cb3c2a275b95cbefc451b9f4
Tommy Qu <tommyqu1992@gmail.com>
2016-05-06 17:01:15 -0400

    Add weather info and change some styles

--- www/js/controller/homeCtrl.js
+++ www/js/controller/homeCtrl.js
@@ -1,15 +1,27 @@
-app.controller('homeCtrl', function($scope, $state, $cordovaGeolocation) {
+app.controller('homeCtrl', function($scope, $state, $cordovaGeolocation, $ionicLoading) {
 
+	$ionicLoading.show({
+      template: 'Loading...'
+    });
 	var posOptions = {timeout: 1000, enableHighAccuracy: false};
+	
 	$cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
 		var lat  = position.coords.latitude;
 		var lon = position.coords.longitude;
-		$.getJSON("http://api.openweathermap.org/data/2.5/forecast/weather?lat="+lat+"&lon="+lon+"&APPID=356cb0594ebdf6b8bf26324cf7ef43c1",function(json){
+		$.getJSON("https://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+lon+"&key=AIzaSyC1qBVXiqFpqwBvjvOpUVoYfTLrkvjBzRo",function(json){
 			var result = JSON.stringify(json);
-			$scope.city = json.city.name;
-			console.log(result);
-			$state.go($state.current, {}, {reload: true});
-			// console.log(result);
+			var address = json.results[1].formatted_address;
+			var city = address.split(",")[0];
+			var state = address.split(",")[1];
+			$scope.address = json.results[1].formatted_address;
+			$.getJSON("https://api.wunderground.com/api/7ae3df327a9e5b28/conditions/q/"+state+"/"+city+".json",function(weatherJson){
+				$scope.temp = weatherJson.current_observation.temperature_string;
+				$scope.humidity = weatherJson.current_observation.relative_humidity;
+				$scope.img = weatherJson.current_observation.icon_url;
+				$scope.date = weatherJson.current_observation.local_time_rfc822.substring(0, 17);
+				$state.go($state.current, {}, {reload: true});
+				$ionicLoading.hide();
+			});
 		});
 	}, function(err) {
       // error
